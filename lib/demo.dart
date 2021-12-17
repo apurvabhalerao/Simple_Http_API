dca89fb53ef66d577f7350f146c4ca0e7454a5a9
apurvabhalerao <apurvabhalerao26@gmail.com>
2021-12-17 13:45:05 +0530

    simple Http API Added

---
+++ lib/demo.dart
@@ -0,0 +1,28 @@
+// import 'dart:html';
+
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart';
+
+// void main(){
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatefulWidget{
+//   @override
+// _MyAppState createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp>{
+// final url="https://jsonplaceholder.typicode.com/posts";
+//   void fetchPosts(){
+// get(url);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+
+//     );
+//   }
+
+// }
